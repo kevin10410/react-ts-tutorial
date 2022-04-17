245812bb6f3380e69168888e6ff20b967f11dab9
kevin10410 <kevin10410@gmail.com>
2022-04-17 16:17:13 +0800

    feat:implement expense state in ExpenseForm

--- src/components/NewExpense/ExpenseForm/index.tsx
+++ src/components/NewExpense/ExpenseForm/index.tsx
@@ -1,20 +1,51 @@
+import React, { useState } from 'react'
 import './index.css'
 
 const ExpenseForm = () => {
+  const [expense, setExpense] = useState({
+    date: '',
+    title: '',
+    amount: '',
+  })
+
+  const dateChangeHandler = (event: React.FormEvent<HTMLInputElement>) => {
+    const { value } = event.currentTarget
+    setExpense(prevState => ({ ...prevState, date: value }))
+  }
+
+  const titleChangeHandler = (event: React.FormEvent<HTMLInputElement>) => {
+    const { value } = event.currentTarget
+    setExpense(prevState => ({ ...prevState, title: value }))
+  }
+
+  const amountChangeHandler = (event: React.FormEvent<HTMLInputElement>) => {
+    const { value } = event.currentTarget
+    setExpense(prevState => ({ ...prevState, amount: value }))
+  }
+
   return (
     <form>
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
-          <input type='text' />
+          <input type='text'
+            value={expense.title}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className='new-expense__control'>
           <label>Amount</label>
-          <input type='number' min='1' step='1' />
+          <input type='number' min='1' step='1'
+            value={expense.amount}
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className='new-expense__control'>
           <label>Date</label>
-          <input type='date' />
+          <input type='date'
+            value={expense.date}
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className='new-expense__actions'>
